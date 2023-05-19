de2fea414c625df5f4729bd2bdc098c946555071
Gabegar <gagarcia190@gmail.com>
2023-05-19 15:37:59 -0400

    added img src(s) to image arr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Images from "./utils/images";
 
 const App = () => {
-    return <>Hello World!</>;
+    const content = Images.map((image) => {
+        return <img key={image.id} src={image.src} alt={image.alt} />;
+    });
+
+    return <>{content}</>;
 };
 
 export default App;
---
+++ src/utils/images.js
@@ -0,0 +1,89 @@
+import kh1 from "../assets/images/KH1.jpg";
+import kh2 from "../assets/images/KH2.5.jpg";
+import kh3 from "../assets/images/KH3.jpg";
+import kh4 from "../assets/images/KH3.5.jpg";
+import kh5 from "../assets/images/KHBBS.jpg";
+import p51 from "../assets/images/P51.png";
+import p5r1 from "../assets/images/P5R1.jpg";
+import p5r2 from "../assets/images/P5R2.jpg";
+import p5s1 from "../assets/images/P5S1.jpg";
+import p5s2 from "../assets/images/P5S2.jpg";
+import ssbu1 from "../assets/images/SSBU1.jpg";
+import ssbu2 from "../assets/images/SSBU2.jpg";
+import ssbu3 from "../assets/images/SSBU3.jpg";
+import ssbu4 from "../assets/images/SSBU4.jpg";
+
+const Images = [
+    {
+        id: 1,
+        src: kh1,
+        alt: "Kingdom Hearts 1 game image",
+    },
+    {
+        id: 2,
+        src: kh2,
+        alt: "Kingdom Hearts 2.5 game image",
+    },
+    {
+        id: 3,
+        src: kh3,
+        alt: "Kingdom Hearts 3 game image #1",
+    },
+    {
+        id: 4,
+        src: kh4,
+        alt: "Kingdom Hearts 3 game image #2",
+    },
+    {
+        id: 5,
+        src: kh5,
+        alt: "Kingdom Hearts Birth By Sleep image",
+    },
+    {
+        id: 6,
+        src: p51,
+        alt: "Persona 5 game image",
+    },
+    {
+        id: 7,
+        src: p5r1,
+        alt: "Persona 5 Royal game image #1",
+    },
+    {
+        id: 8,
+        src: p5r2,
+        alt: "Persona 5 Royal game image #2",
+    },
+    {
+        id: 9,
+        src: p5s1,
+        alt: "Persona 5 Strikers game image #1",
+    },
+    {
+        id: 10,
+        src: p5s2,
+        alt: "Persona 5 Strikers game image #2",
+    },
+    {
+        id: 11,
+        src: ssbu1,
+        alt: "Super Smash Bros Ultimate game image #1",
+    },
+    {
+        id: 12,
+        src: ssbu2,
+        alt: "Super Smash Bros Ultimate game image #2",
+    },
+    {
+        id: 13,
+        src: ssbu3,
+        alt: "Super Smash Bros Ultimate game image #3",
+    },
+    {
+        id: 14,
+        src: ssbu4,
+        alt: "Super Smash Bros Ultimate game image #4",
+    },
+];
+
+export default Images;
